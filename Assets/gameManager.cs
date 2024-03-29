f8d16641e9204dc9c358dcdfd49f521d81e38453
greatoni <greatoni3@gmail.com>
2024-03-29 16:39:42 -0400

    Keys and doors added

---
+++ Assets/gameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager gm;
+
+    public level1Concierge levelConcierge;
+
+
+    private void Awake()
+    {
+        if(gm != null && gm != this)
+        {
+            Destroy(this); 
+        }
+        else 
+        {
+            DontDestroyOnLoad(this);
+            gm = this;
+        }
+
+        levelConcierge = GetComponentInChildren<level1Concierge>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gm != null && gm != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+            gm = this;
+        }
+
+        levelConcierge = GetComponentInChildren<level1Concierge>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
